12a46713b6b132cab8aedfd8e03d4d49c744afa7
Nam Phong <phongnguyennam@zamba.vn>
2015-06-26 22:40:48 +0700

    create project

---
+++ 1Again/1Again/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1Again
+//
+//  Created by Nam Phong on 6/26/15.
+//  Copyright (c) 2015 Phong Nguyen Nam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
